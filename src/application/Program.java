6f0e3030954dc13750d7a87f28a967e0a7aa1095
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-13 15:06:08 -0300

    get list to upperCase with map

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,8 +2,10 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCasaName;
 
 public class Program {
 
@@ -16,7 +18,10 @@ public class Program {
 		list.add(new Product("Xbox-One", 1250.00));
 		list.add(new Product("Notebook", 2999.90));
 		list.add(new Product("Acer Nitro", 7280.90));
-
+		
+		List<String> names = list.stream().map(new UpperCasaName()).collect(Collectors.toList());
+		
+		names.forEach(System.out::println);
 	}
 
 }
